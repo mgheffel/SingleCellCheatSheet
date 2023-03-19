4c987f0afda479ab75f110bb9c6e03dbb31b2550
mgheffel <mgheffel@gmail.com>
2023-03-19 13:11:48 -0700

    Update DataFrame_Operations.py

--- DataFrame_Operations.py
+++ DataFrame_Operations.py
@@ -3,3 +3,9 @@ min_value = df[col].min()
 max_value = df[col].max()
 df[col] = df[col] - min_value
 df[col] = df[col] / (max_value - min_value)
+
+for col in ['mCH/CH','mCG/CG']:
+    min_value = df[col].min()
+    max_value = df[col].max()
+    df[col] = df[col] - min_value
+    df[col] = df[col] / (max_value - min_value)
