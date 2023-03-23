15a2609068717b20fb8c0173fab9620c6d4fb15c
mgheffel <mgheffel@gmail.com>
2023-03-23 12:15:39 -0700

    Update DataFrame_Operations.py

--- DataFrame_Operations.py
+++ DataFrame_Operations.py
@@ -4,3 +4,22 @@ def normalize_col_zero_one(df,col):
     max_value = pdf[col].max()
     df[col] = df[col] - min_value
     df[col] = df[col] / (max_value - min_value)
+    
+    
+chunks_dict={}
+chunks_dict['CA']=5
+chunks_dict['Astro']=4
+chunks_dict['GC']=4
+chunks_dict['OPC']=2
+chunks_dict['ODC']=2
+for i in pdata.obs['dptL3'].unique():
+    name=i
+    if i in chunks_dict.keys():
+        n_chunks=chunks_dict[i]
+    else:
+        n_chunks=1
+    chunks = np.array_split(pdata[pdata.obs['dptL3']==name].obs.sort_values('inverted_dpt'), n_chunks)
+    c=0
+    for i in chunks:
+        pdata.obs.loc[i.index, 'pdpt'] = name+'_'+str(c)
+        c+=1
