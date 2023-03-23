beb228af5341e8d4b2c23ddaa5442e10633b0ecf
mgheffel <mgheffel@gmail.com>
2023-03-23 12:11:55 -0700

    Update DataFrame_Operations.py

--- DataFrame_Operations.py
+++ DataFrame_Operations.py
@@ -1,11 +1,6 @@
 #Normalize to zero one 
-min_value = df[col].min()
-max_value = df[col].max()
-df[col] = df[col] - min_value
-df[col] = df[col] / (max_value - min_value)
-
-for col in ['mCH/CH','mCG/CG']:
-    min_value = df[col].min()
-    max_value = df[col].max()
+def normalize_col_zero_one(df,col):
+    min_value = pdf[col].min()
+    max_value = pdf[col].max()
     df[col] = df[col] - min_value
     df[col] = df[col] / (max_value - min_value)
