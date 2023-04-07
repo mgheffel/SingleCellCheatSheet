4d6731ae08d75bd981f3ff5c32723dba42cefa7c
mgheffel <mgheffel@gmail.com>
2023-04-07 11:47:55 -0700

    Update ScanpyManipulation.py

--- ScanpyManipulation.py
+++ ScanpyManipulation.py
@@ -3,8 +3,8 @@ adata.obs['temp_clust']=adata2.obs['leiden2'].apply(lambda i: clust_map[i] if i 
 adata.obs['nL2']=np.where(adata.obs['temp_clust'].isnull(),adata.obs['nL2'],adata.obs['temp_clust'])
 
 #harmony integration
-sc.external.pp.harmony_integrate(adata2, 'batch',max_iter_harmony=20)
-sc.pp.neighbors(adata2,n_pcs=20,n_neighbors=20,use_rep='X_pca_harmony')
+sc.external.pp.harmony_integrate(adata, 'batch',max_iter_harmony=20)
+sc.pp.neighbors(adata,n_pcs=20,n_neighbors=20,use_rep='X_pca_harmony')
 
 
 
