fa2815051ae3e0ea3706a37902e434a88cdc5085
mgheffel <mgheffel@gmail.com>
2023-04-15 16:43:14 -0700

    Update DataFrame_Operations.py

--- DataFrame_Operations.py
+++ DataFrame_Operations.py
@@ -1,7 +1,7 @@
 #Normalize to zero one 
 def normalize_col_zero_one(df,col):
-    min_value = pdf[col].min()
-    max_value = pdf[col].max()
+    min_value = df[col].min()
+    max_value = df[col].max()
     df[col] = df[col] - min_value
     df[col] = df[col] / (max_value - min_value)
     
