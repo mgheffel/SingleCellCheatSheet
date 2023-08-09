e1673d4d0ad9b68f0ae32e83f898fb71af001048
mgheffel <mgheffel@gmail.com>
2023-08-09 13:32:47 -0700

    update umap manipulation

--- Plotting.py
+++ Plotting.py
@@ -6,16 +6,12 @@ matplotlib.rcParams['pdf.fonttype']=42
 plt.rcParams['font.family']='Verdana'
 
 #UMAP / draw_graph / scatter plot coordinate manipulation framework
+#rotate umap -180 degrees (left)
 adata.obsm['X_draw_graph_fa_mC']=adata.obsm['X_draw_graph_fa'][:, [1, 0]]
-adata.obsm['X_pca_3C']=adata2.obsm['X_pca']
-array=adata2.obsm['X_draw_graph_fa'][:, [1, 0]].copy()
+#flip x-axis
+array=adata2.obsm['X_draw_graph_fa_3C'][:, [1, 0]].copy()
 array[:, 0] = -array[:, 0]
 adata.obsm['X_draw_graph_fa_3C'] = array
-array=adata2.obsm['X_umap'][:, [1, 0]].copy()
-array[:, 0] = -array[:, 0]
-adata.obsm['X_umap_3C']=array
-#[:, [1, 0]]*-1
-adata.obs['3C_dpt']=adata2.obs['dpt_pseudotime']
 
 
 #expects 2d dataframe of fractions
